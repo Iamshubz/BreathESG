c6fe9b8869f2e7302bd4d8129d45a604cc94f49c
Iamshubz <119273013+Iamshubz@users.noreply.github.com>
2026-05-28 19:38:03 +0530

    Fix CORS and CSRF for production frontend

--- backend/breathe_esg/settings.py
+++ backend/breathe_esg/settings.py
@@ -70,3 +70,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://breath-esg-eight.vercel.app"
+] 
